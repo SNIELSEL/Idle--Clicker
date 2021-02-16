91892fe13868bf18802134573471e79a871c5f75
SNIELSEL <79170413+SNIELSEL@users.noreply.github.com>
2021-02-16 21:58:13 +0100

    romoved libarary

---
+++ Assets/scripts/Controller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using TMPro;
+using System;
+using BreakInfinity;
+
+public class Controller : MonoBehaviour
+{
+    public Data data = new Data();
+
+    public TMP_Text CoinsText;
+
+   public void start()
+    {
+    }
+
+    public void Update()
+    {
+        CoinsText.text = $"{data.coins}";
+    }
+
+    public void GenerateCoins()
+    {
+        data.coins += 1;
+    }
+}
---
+++ Assets/scripts/Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using BreakInfinity;
+public class Data
+{
+    public BigDouble coins = 0;
+
+    public Data()
+    {
+    }
+}
